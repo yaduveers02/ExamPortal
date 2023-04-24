020a2ee79f932d57e685d2bffbb972d053d132d8
Yaduveer Singh <yaduveers02@gmail.com>
2023-04-24 16:20:20 +0530

    adding security

--- examserver/src/main/java/com/exam/examserver/entity/UserRole.java
+++ examserver/src/main/java/com/exam/examserver/entity/UserRole.java
@@ -27,23 +27,18 @@ public class UserRole {
 	public long getUserRoleId() {
 		return userRoleId;
 	}
-
 	public void setUserRoleId(long userRoleId) {
 		this.userRoleId = userRoleId;
 	}
-
 	public User getUser() {
 		return user;
 	}
-
 	public void setUser(User user) {
 		this.user = user;
 	}
-
 	public Role getRole() {
 		return role;
 	}
-
 	public void setRole(Role role) {
 		this.role = role;
 	}
